c68d84bdcc2bf7771e5ec37285cfe997aac8b42d
atlashodl <94209186+AtlasHODL@users.noreply.github.com>
2022-12-24 14:39:15 -0500

    1st commit.

---
+++ kcwebsite/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '..src/App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to your website KC, time to start your journey as a dev.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
